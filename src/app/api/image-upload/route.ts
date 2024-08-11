bdaa8ec8905a9180ae508d7decc28551689bbb1a
arpit <arpit2003y@gmail.com>
2024-08-11 11:00:13 +0530

    feat(api):added_asset_upload_route

---
+++ src/app/api/image-upload/route.ts
@@ -0,0 +1,68 @@
+import { NextRequest, NextResponse } from "next/server"
+import { v2 as cloudinary, UploadStream } from "cloudinary"
+import { auth } from "@clerk/nextjs/server"
+
+cloudinary.config({
+    cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+})
+
+interface CloudinaryUploadResult {
+    public_id: string
+    [key: string]: any
+}
+
+export async function POST(request: NextRequest) {
+    const { userId } = auth()
+
+    if (!userId) {
+        return NextResponse.json({ error: "Unauthorized" }, { status: 401 })
+    }
+
+    try {
+        const formData = await request.formData()
+        const file = (formData.get("file") as File) || null
+
+        if (!file) {
+            return NextResponse.json({ error: "Missing file" }, { status: 400 })
+        }
+
+        const bytes = await file.arrayBuffer()
+        const buffer = Buffer.from(bytes)
+
+        const response = await new Promise<CloudinaryUploadResult>(
+            (resolve, reject) => {
+                const uploadStream = cloudinary.uploader.upload_stream(
+                    {
+                        folder: "cclip",
+                    },
+                    (error, result) => {
+                        if (error) reject(error)
+                        else resolve(result as CloudinaryUploadResult)
+                    }
+                )
+                uploadStream.end(buffer)
+            }
+        )
+
+        return NextResponse.json(
+            {
+                publicId: response.public_id,
+            },
+            {
+                status: 200,
+            }
+        )
+    } catch (error) {
+        console.log("Image upload failed:", error)
+        return NextResponse.json(
+            {
+                error: "Image Upload failed",
+            },
+            {
+                status: 500,
+            }
+        )
+    }
+}
